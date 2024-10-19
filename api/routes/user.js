56d0f28a28d2cbc7ec7adec4c5fa8823f983da76
Trực Nguyễn Hữu <truc95520@gmail.com>
2024-10-19 20:31:28 +0700

    hello

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const checkAuth = require("../middleware/check-auth");
 const userController = require("../controller/user/user");
 const upload = require("../middleware/img-multer");
-router.get("/", checkAuth, userController.get_all_user);
+router.get("/", userController.get_all_user);
 router.post("/signup", userController.create_user);
 router.post("/forgotPassword", userController.forgot_password);
 router.post("/resetPassword/:id/:token", userController.reset_password);
@@ -13,5 +13,5 @@ router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.delete("/:userId", userController.delete_user);
 
-router.post("/uploadImage", upload.single("avatar"), userController.upload_img);//ảnh đại diện user,chưa làm get 
+router.post("/uploadImage", upload.single("avatar"), userController.upload_img); //ảnh đại diện user,chưa làm get
 module.exports = router;
--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    //   [
-    //   "http://localhost:5173",
-    //   "http://192.168.1.38:5173",
-    //   "https://5b69-2405-4802-95c1-5e90-f02f-2c02-4429-f0d1.ngrok-free.app",
-    // ],
+    origin: [
+      "https://fe-pbl4-ytsx.vercel.app",
+      "http://192.168.1.38:5173",
+      "https://5b69-2405-4802-95c1-5e90-f02f-2c02-4429-f0d1.ngrok-free.app",
+    ],
   },
 });
 
