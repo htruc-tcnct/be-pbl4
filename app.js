3b84488987b2fc335a5e8b646bda645acb16b8e7
Trực Nguyễn Hữu <truc95520@gmail.com>
2024-10-21 17:28:16 +0700

    localhost normal

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const documentRoutes = require("./api/routes/document");
 connectDB();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://fe-pbl4-ytsx.vercel.app/"],
+  origin: ["http://localhost:5173", "https://fe-pbl4-ytsx.vercel.app"],
   credentials: true,
 };
 app.use(cors(corsOptions));
