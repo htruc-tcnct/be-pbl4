6420682c68e7a80f4211fff7406e511016cd8560
Trực Nguyễn Hữu <truc95520@gmail.com>
2024-10-24 18:18:47 +0700

    thuat toan ot

--- index.js
+++ index.js
@@ -1,46 +1,38 @@
 const http = require("http");
 const app = require("./app");
-const { Server } = require("socket.io");
-const ot_toy = require("./ot_toy");
+
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
-const docState = new ot_toy.DocState();
+const { Server } = require("socket.io");
+var ot_toy = require("./ot_toy");
+const { json } = require("body-parser");
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173", "https://fe-pbl4-ytsx.vercel.app"],
+    credentials: true,
+  },
+});
+var docState = new ot_toy.DocState();
 
-let rev = 0;
-
+var rev = 0;
 function broadcast() {
   if (rev < docState.ops.length) {
     io.emit("update", docState.ops.slice(rev));
     rev = docState.ops.length;
   }
 }
-
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://fe-pbl4-ytsx.vercel.app",
-      "http://192.168.1.4:5173", // Thêm địa chỉ IP nội bộ
-    ],
-    credentials: true, // Cho phép gửi cookies nếu cần
-  },
-});
-
 io.on("connection", function (socket) {
-  const peer = new ot_toy.Peer(); // Tạo peer mới cho mỗi kết nối client
+  var peer = new ot_toy.Peer();
   console.log("client connected");
-
   socket.on("update", function (ops) {
-    for (let i = 0; i < ops.length; i++) {
+    for (var i = 0; i < ops.length; i++) {
       peer.merge_op(docState, ops[i]);
     }
     broadcast();
     console.log("update: " + JSON.stringify(ops) + ": " + docState.get_str());
   });
-
   socket.emit("update", docState.ops);
 });
-
 server.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
